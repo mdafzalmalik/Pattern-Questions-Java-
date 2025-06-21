068e283b3bd5f64636f3ec409a0907b69a7ba1c5
Mohammad Afzal Malik <mdafzalmalik13@gmail.com>
2025-06-21 22:50:52 +0530

    Add files via upload

---
+++ Pattern19.java
@@ -0,0 +1,53 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern19 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int row = 1;
+		int no_of_spaces = -1;
+		int no_of_stars = n/2+1;
+		while(row <= n) {
+			//printing 1st star pattern
+			int star = 1;
+			while(star <= no_of_stars) {
+				System.out.print("* ");
+				star++;
+			}
+			// printing spaces
+			int space = 1;
+			while(space <= no_of_spaces) {
+				System.out.print("  ");
+				space++;
+			}
+			// printing 2nd star pattern
+			int star_ = 1;
+			if(row == 1) {
+				star_ = 2;
+			} else if(row == n) {
+				star_ = 2;
+			}
+			while(star_ <= no_of_stars) {
+				System.out.print("* ");
+				star_++;
+			}
+			// next row
+			System.out.println();
+			// mirroring
+			if(row < n/2+1) {
+				no_of_spaces += 2;
+				no_of_stars--;
+			} else {
+				no_of_spaces -= 2;
+				no_of_stars++;
+			}
+			row++;
+		}
+
+	}
+
+}
